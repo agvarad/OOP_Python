934a8070e6c50277330593ac44c8b7dd9aad0cdf
Varadarajan <telxsibelkin.ios@gmail.com>
2015-07-10 16:02:08 +0530

    Extend Builtins

---
+++ ExtendClass_Builtins.py
@@ -0,0 +1,23 @@
+__author__ = 'Python Object Oriented Programming'
+
+
+class ContactDict(dict):
+    def search(self, name):
+        matching = []
+        for data in self:
+            if name in data:
+                matching.append(name)
+        return matching
+
+
+class Contact:
+    all_contacts = ContactDict()
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+
+if __name__ == "__main__":
+    C = Contact('varadarajan', 'varadag@tataelxsi.co.in')
+    print C.all_contacts
