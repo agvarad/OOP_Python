09fff8f2e17e48d9257b8234e13bd5ea1229a473
Varadarajan <telxsibelkin.ios@gmail.com>
2015-07-10 15:51:17 +0530

    Updated Simple Inherritance - removed errors

--- Simple_Inherritance.py
+++ Simple_Inherritance.py
@@ -3,22 +3,23 @@ __author__ = 'Python Object Oriented Programming'
 
 class News:
     def __init__(self):
-        self.Headlines_Today = ['India wins world cup','Threat at the Border','Big data and Hadoop and happy minds.']
-        self.Headlines_Yesterday = ['India on big final','India-Pak border issue continues','What is new in computers']
+        self.Headlines_Today = ['India wins world cup', 'Threat at the Border', 'Big data and Hadoop and happy minds.']
+        self.Headlines_Yesterday = ['India on big final', 'India-Pak border issue continues', 'What is new in computers']
 
-    def todaysnews(self):
+    def today_news(self):
         for headlines in self.Headlines_Today:
             print headlines
 
-    def yesterdaysnews(self):
+    def yesterday_news(self):
         for headlines in self.Headlines_Yesterday:
             print headlines
 
+
 class Hindu(News):
-    pass
+    def __init__(self):
+        pass
+
 
 if __name__ == "__main__":
     H = Hindu()
-    print H.todaysnews()
-
-
+    print H.today_news()
